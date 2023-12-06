6cc6056464a6241baaeca197179ef8addd34e03a
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2023-12-06 12:18:11 +0300

    Update default_client.go

--- src/gostratum/default_client.go
+++ src/gostratum/default_client.go
@@ -111,15 +111,15 @@ func SendExtranonce(ctx *StratumContext) {
 	}
 }
 
-var walletRegex = regexp.MustCompile("kaspa:[a-z0-9]+")
+var walletRegex = regexp.MustCompile("pyrin:[a-z0-9]+")
 
 func CleanWallet(in string) (string, error) {
 	_, err := util.DecodeAddress(in, util.Bech32PrefixKaspa)
 	if err == nil {
 		return in, nil // good to go
 	}
-	if !strings.HasPrefix(in, "kaspa:") {
-		return CleanWallet("kaspa:" + in)
+	if !strings.HasPrefix(in, "pyrin:") {
+		return CleanWallet("pyrin:" + in)
 	}
 
 	// has kaspa: prefix but other weirdness somewhere
